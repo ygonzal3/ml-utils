0d690531e9c655f664452572c35c635bec5ccdb9
ygonzal3 <gonzalezyoel@bellsouth.net>
2024-06-02 23:15:26 -0400

    update dir structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+import re
+from setuptools import setup, find_packages
+
+
+def get_version_and_author(package):
+    with open(package + '/__init__.py', 'r') as f:
+        init_py = f.read()
+    version = re.search(r"__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    author = re.search(r"__author__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    return version, author
+
+
+version, author = get_version_and_author('ml_utils')
+
+setup(
+    name='ml-utils',
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    url='https://github.com/ygonzal3/ml-utils',
+    author=author,
+    author_email='ygonzalez112@gmail.com',
+    description='ML helper classes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
